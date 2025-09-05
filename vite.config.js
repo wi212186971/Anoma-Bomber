e8bae699173d10d341b919dcabf41fb448cad914
wi212186971 <wi212186971@users.noreply.github.com>
2025-09-04 20:16:59 -0400

    Update Vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,15 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Anoma-Bomber/',
-  plugins: [react(),tailwindcss()],
+  base: process.env.NODE_ENV === 'production' ? '/Anoma-Bomber/' : '/',
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
 })
+
+
