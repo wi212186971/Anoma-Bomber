7898e672533cce5d6ad45c39b07c4ebe81953e85
wi212186971 <wi212186971@users.noreply.github.com>
2025-09-04 20:59:20 -0400

    Set Vite base path to empty string for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Anoma-Bomber/',
+  base: '',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
