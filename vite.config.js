17dc63e1fc51d26051b07f44bbd190ccab467265
wi212186971 <wi212186971@users.noreply.github.com>
2025-09-04 20:30:50 -0400

    Update Vite base path to relative for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Anoma-Bomber/' : '/',
+  base: './',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
